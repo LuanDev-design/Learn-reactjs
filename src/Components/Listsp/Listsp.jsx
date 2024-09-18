17a3ff480e0af645fb3005aa1ad6ccbc86152a97
LuanDev-design <thanhluanwebdesign@gmail.com>
2024-09-18 14:28:20 +0700

    Cập nhật

--- src/Components/Listsp/Listsp.jsx
+++ src/Components/Listsp/Listsp.jsx
@@ -3,15 +3,16 @@ import { listsp } from '../../data'
 
 export default function Listsp() {
   return (
-    <ul>
-        {listsp.map(sp => (
-            <div key={sp.id}>
-               <li> {sp.title}</li>
+    <div className='home'>
+        {listsp.map((sp, i) => (
+            <div className="sp" key={i}>
+               <h2> {sp.title}</h2>
                <p> {sp.time} </p>
+               <img src={sp.image} alt={sp.title} />
                 
                 </div>
             
         ) )}
-    </ul>
-  )
+    </div>
+  );
 }
--- src/data.js
+++ src/data.js
@@ -2,18 +2,21 @@ export  const listsp = [
     {
         id:1,
         title: "Eat",
-        time: 20
+        time: 20,
+        image: "https://cloudify.vn/wp-content/uploads/2021/11/code.jpg"
      },
 
      {
         id:2,
         title: "Sleep",
-        time: 30
+        time: 30,
+        image: "https://www.economist.com/sites/default/files/images/2015/09/blogs/economist-explains/code2.png"
     },
 
     {
         id:3,
         title: "Code",
-        time: 20
+        time: 20,
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTc-06OgWFsxqAL9uNIW1Bf8wr94Mau8v2IaA&s"
     },
 ]
